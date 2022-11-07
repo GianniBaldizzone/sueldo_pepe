0c9c4b22d3aaeba1285fd5d3586963c8b927950e
escuela@davinci.edu.ar <gianni.baldizzone@davinci.edu.ar>
2022-11-07 12:31:08 -0300

    feat: implemens cadete class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args){
         System.out.println("Sueldo de pepe Argento");
-    }
+        Cadete pepe = new Cadete();
+        System.out.println(String.format("El sueldo total del empleado es %f",
+                pepe.calcularSueldoNeto()
+        ));
+}
 }
